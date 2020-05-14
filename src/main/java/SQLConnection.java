f26e2156bc7cc483582b5bfbd836781d1776deb2
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-05-14 20:57:04 +0200

    Modified connection class to avoid starting connection over and over

--- src/main/java/SQLConnection.java
+++ src/main/java/SQLConnection.java
@@ -2,12 +2,26 @@ import java.sql.*;
 
 public class SQLConnection {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String url = "jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=CONNECTIS;integratedSecurity=true;";
-        try(Connection connection = DriverManager.getConnection(url)) {
-            //use connection
-            System.out.println("Connection is valid (500ms): " + connection.isValid(500));
+    private Connection connection;
+
+    public Connection connectionToSQLServer() {
+
+        String databaseName = "ExampleJDBC";
+        String url = "jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=" + databaseName + ";integratedSecurity=true;";
+
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url);
+            System.out.println(connection);
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
+
+        return connection;
+    }
+
+    public Connection getConnection() {
+        return (connection != null) ? connection : connectionToSQLServer();
     }
 }
